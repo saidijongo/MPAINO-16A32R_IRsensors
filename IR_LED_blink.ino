9928bf5ffd02fc5203bd8cb7efe53608a4667442
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-11-05 23:50:43 +0900

    Create IR_LED_blink.ino

---
+++ IR_LED_blink.ino
@@ -0,0 +1,39 @@
+const int totalLEDs = 32;
+int ledPins[totalLEDs];
+int sensor1Pin = 2;
+int sensor2Pin = 3; 
+
+void setup() {
+  for (int pin = 0; pin < totalLEDs; pin++) {
+    ledPins[pin] = 54 + pin;
+    pinMode(ledPins[pin], OUTPUT);
+  }
+
+  pinMode(sensor1Pin, INPUT);
+  pinMode(sensor2Pin, INPUT);
+}
+
+void loop() {
+  int sensor1State = digitalRead(sensor1Pin);
+
+  if (sensor1State == HIGH) {
+    // Sensor 1 is interrupted, blink LEDs
+    for (int pin = 0; pin < totalLEDs; pin++) {
+      digitalWrite(ledPins[pin], HIGH);
+    }
+  } else {
+    // Sensor 1 is not interrupted, turn off LEDs
+    for (int pin = 0; pin < totalLEDs; pin++) {
+      digitalWrite(ledPins[pin], LOW);
+    }
+  }
+
+  int sensor2State = digitalRead(sensor2Pin);
+
+  if (sensor2State == HIGH) {
+    // Sensor 2 is interrupted, turn off blinking LEDs
+    for (int pin = 0; pin < totalLEDs; pin++) {
+      digitalWrite(ledPins[pin], LOW);
+    }
+  }
+}
